4e0e421a42d78eb570ece161334f16758c5dc4c9
MikeFredley <36174846+MikeFredley@users.noreply.github.com>
2021-04-13 21:03:29 -0400

    Add files via upload

---
+++ Facial Recognition/Facial Recognition/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Facial_Recognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picCapture = new System.Windows.Forms.PictureBox();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.btnDetectFaces = new System.Windows.Forms.Button();
+            this.txtPersonName = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnTrain = new System.Windows.Forms.Button();
+            this.picDetected = new System.Windows.Forms.PictureBox();
+            this.btnAddPerson = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picCapture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDetected)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picCapture
+            // 
+            this.picCapture.Location = new System.Drawing.Point(12, 9);
+            this.picCapture.Name = "picCapture";
+            this.picCapture.Size = new System.Drawing.Size(676, 426);
+            this.picCapture.TabIndex = 0;
+            this.picCapture.TabStop = false;
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Location = new System.Drawing.Point(694, 12);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(149, 23);
+            this.btnCapture.TabIndex = 1;
+            this.btnCapture.Text = "1. Capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // btnDetectFaces
+            // 
+            this.btnDetectFaces.Location = new System.Drawing.Point(695, 42);
+            this.btnDetectFaces.Name = "btnDetectFaces";
+            this.btnDetectFaces.Size = new System.Drawing.Size(148, 23);
+            this.btnDetectFaces.TabIndex = 2;
+            this.btnDetectFaces.Text = "2. Detect Faces";
+            this.btnDetectFaces.UseVisualStyleBackColor = true;
+            this.btnDetectFaces.Click += new System.EventHandler(this.btnDetectFaces_Click);
+            // 
+            // txtPersonName
+            // 
+            this.txtPersonName.Location = new System.Drawing.Point(695, 274);
+            this.txtPersonName.Name = "txtPersonName";
+            this.txtPersonName.Size = new System.Drawing.Size(148, 20);
+            this.txtPersonName.TabIndex = 3;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(694, 301);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(149, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnTrain
+            // 
+            this.btnTrain.Location = new System.Drawing.Point(695, 331);
+            this.btnTrain.Name = "btnTrain";
+            this.btnTrain.Size = new System.Drawing.Size(148, 23);
+            this.btnTrain.TabIndex = 5;
+            this.btnTrain.Text = "4. Train Images";
+            this.btnTrain.UseVisualStyleBackColor = true;
+            this.btnTrain.Click += new System.EventHandler(this.btnTrain_Click);
+            // 
+            // picDetected
+            // 
+            this.picDetected.Location = new System.Drawing.Point(694, 115);
+            this.picDetected.Name = "picDetected";
+            this.picDetected.Size = new System.Drawing.Size(164, 153);
+            this.picDetected.TabIndex = 7;
+            this.picDetected.TabStop = false;
+            // 
+            // btnAddPerson
+            // 
+            this.btnAddPerson.Location = new System.Drawing.Point(695, 71);
+            this.btnAddPerson.Name = "btnAddPerson";
+            this.btnAddPerson.Size = new System.Drawing.Size(149, 23);
+            this.btnAddPerson.TabIndex = 8;
+            this.btnAddPerson.Text = "3. Add Person";
+            this.btnAddPerson.UseVisualStyleBackColor = true;
+            this.btnAddPerson.Click += new System.EventHandler(this.btnAddPerson_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(695, 360);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(164, 153);
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(871, 535);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.btnAddPerson);
+            this.Controls.Add(this.picDetected);
+            this.Controls.Add(this.btnTrain);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtPersonName);
+            this.Controls.Add(this.btnDetectFaces);
+            this.Controls.Add(this.btnCapture);
+            this.Controls.Add(this.picCapture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picCapture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDetected)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picCapture;
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.Button btnDetectFaces;
+        private System.Windows.Forms.TextBox txtPersonName;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnTrain;
+        private System.Windows.Forms.PictureBox picDetected;
+        private System.Windows.Forms.Button btnAddPerson;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Facial Recognition/Facial Recognition/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Face;
+using Emgu.CV.CvEnum;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using Emgu.Util;
+
+namespace Facial_Recognition
+{
+    public partial class Form1 : Form
+    {
+        #region Variables
+        private Capture videoCapture = null;
+        private Image<Bgr, Byte> currentFrame = null;
+        Mat frame = new Mat();
+        private bool facesDetectionEnabled = false;
+        CascadeClassifier faceCascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt.xml");
+      /*  List<Image<Gray, Byte>> TrainedFaces = new List<Image<Gray, byte>>();
+        List<int> PersonLabes = new List<int>(); */
+        bool EnableSaveImage = false;
+        Saving save;
+        private static bool isTrained = false;
+
+        Image<Gray, Byte>[] trainingImages;
+        Image<Gray, Byte>[] trainingSizedImages;
+        int imagesCount;
+        int[] labels;
+        EigenFaceRecognizer recognizer;
+        string[] PersonName;
+
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            videoCapture = new Capture();
+            videoCapture.ImageGrabbed += ProcessFrame;
+            videoCapture.Start();
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            // Step 1: Video Capture
+            videoCapture.Retrieve(frame, 0);
+            currentFrame = frame.ToImage<Bgr, Byte>().Resize(picCapture.Width, picCapture.Height, Inter.Cubic);
+
+            // Step 2: Face Detection
+            if (facesDetectionEnabled)
+            {
+                // Convert from Bgr to Gray Image
+                Mat grayImage = new Mat();
+                CvInvoke.CvtColor(currentFrame, grayImage, ColorConversion.Bgr2Gray);
+                // Enhance the image to get better result
+                CvInvoke.EqualizeHist(grayImage, grayImage);
+
+                Rectangle[] faces = faceCascadeClassifier.DetectMultiScale(grayImage, 1.1, 3, Size.Empty, Size.Empty);
+                // If Faces detected
+                if (faces.Length > 0)
+                {
+                    // Draw a square around the face
+                    foreach (var face in faces)
+                    {
+                        CvInvoke.Rectangle(currentFrame, face, new Bgr(Color.Red).MCvScalar, 2);
+
+                        // Step 3: Add Person
+                        // Assign the face to the picture Box face picDetected
+                        Image<Bgr, byte>  resultImage = currentFrame.Convert<Bgr, byte>();
+                        resultImage.ROI = face;
+                        picDetected.SizeMode = PictureBoxSizeMode.StretchImage;
+                        picDetected.Image = resultImage.Bitmap;
+
+                        if (EnableSaveImage)
+                        {
+                            save.setResultImage(resultImage);                                            
+                        }
+
+                        // Show the results for the trained images
+                        if (isTrained)
+                        {
+                            
+                            Image<Gray, Byte> grayFaceResult = resultImage.Convert<Gray, byte>().Resize(200,200, Inter.Cubic);
+                            //var result = recognizer.Predict(grayFaceResult);
+                            EigenFaceRecognizer.PredictionResult res = recognizer.Predict(grayFaceResult);
+                            pictureBox3.Image = trainingSizedImages[res.Label].Bitmap;
+                            Console.WriteLine(res.Label);
+                            Console.WriteLine(res.Distance);
+                            if (res.Distance < 100000 / 10)
+                            {
+                                CvInvoke.PutText(currentFrame, PersonName[res.Label], new Point(face.X - 2, face.Y - 2),
+                                    FontFace.HersheyComplex, 1.0, new Bgr(Color.Orange).MCvScalar);
+                            }
+                            else
+                            {
+                                CvInvoke.PutText(currentFrame, "Unknown", new Point(face.X - 2, face.Y - 2),
+                                    FontFace.HersheyComplex, 1.0, new Bgr(Color.Orange).MCvScalar);
+                            }
+                        }
+                    }
+                }
+            }
+            // Render the video capture into the Picturebox Capture
+            picCapture.Image = currentFrame.Bitmap;
+        }
+
+        private void btnDetectFaces_Click(object sender, EventArgs e)
+        {
+            facesDetectionEnabled = true;
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+          //  btnSave.Enabled = false;
+           // btnAddPerson.Enabled = false;
+            EnableSaveImage = false;
+            save.setSaveImageEnabled(EnableSaveImage);
+        }
+
+        private void btnAddPerson_Click(object sender, EventArgs e)
+        {
+          //  btnSave.Enabled = true;
+          //  btnAddPerson.Enabled = false;
+            
+            // The only way i could get saving to work the way i wanted it to was by creating another object
+            // then using the thread with the object
+            save = new Saving();
+            save.setPersonName(txtPersonName.Text);
+            Thread saving = new Thread(new ThreadStart(save.Save));
+            saving.Start();
+            save.setSaveImageEnabled(true);
+            EnableSaveImage = true;
+            
+            // Thread saveThread = new Thread( new ThreadStart(saveImage));
+        }
+
+        private void btnTrain_Click(object sender, EventArgs e)
+        {
+            TrainImagesFromDir();
+        }
+
+        private void TrainImagesFromDir()
+        {
+            // http://www.emgu.com/wiki/files/3.1.0/document/html/c9c13b1e-4f15-31ea-9d40-4a76d5bee079.htm
+
+            
+            Size imageSize = new Size(200, 200);
+            double Threshold = 100000;
+           // TrainedFaces.Clear();
+          //  PersonLabes.Clear();
+           try
+           {
+                string path = Directory.GetCurrentDirectory() + @"\TrainedImages";
+                string[] files = Directory.GetFiles(path, "*.jpg", SearchOption.AllDirectories);
+                imagesCount = files.Length;
+                trainingImages = new Image<Gray, byte>[imagesCount];
+                trainingSizedImages = new Image<Gray, byte>[imagesCount];
+                labels = new int[imagesCount];
+                PersonName = new string[imagesCount];
+                // Adds all of the images to the array and resizes them all to the same size
+                for (int i = 0; i < files.Length; i++)
+                {
+                    trainingImages[i] = new Image<Gray, byte>(files[i]);
+                    trainingSizedImages[i] = new Image<Gray, byte>(files[i]);
+                    CvInvoke.Resize(trainingImages[i],trainingSizedImages[i], imageSize);
+                    labels[i] = i+1;
+                    PersonName[i] = "Michael";
+                }
+                recognizer = new EigenFaceRecognizer(imagesCount, Threshold);
+                // recognizer.Train(TrainedFaces.ToArray(), PersonLabes.ToArray());
+                recognizer.Train(trainingSizedImages, labels);
+                isTrained = true;
+                Debug.WriteLine(imagesCount);
+                Debug.WriteLine(isTrained);
+               
+           }
+           catch(Exception ex)
+           {             
+                MessageBox.Show("Error in Trained Images: " + ex.Message);
+                
+            }
+        }
+    }
+}
